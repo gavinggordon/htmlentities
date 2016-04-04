61eea717509eab761257ccf31206d037f18a5c23
Gavin G Gordon <gmarkowski@my.bcit.ca>
2016-04-03 20:02:54 -0700

    Attempt to Fix travis.yml
    Attempt 25

--- tests/Html/HtmlEntitiesTest.php
+++ tests/Html/HtmlEntitiesTest.php
@@ -2,7 +2,7 @@
 
 namespace GGGTests;
 
-//use GGG\Html\HtmlEntities;
+use GGG\Html\HtmlEntities;
 
 class HtmlEntitiesTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,7 +13,7 @@ class HtmlEntitiesTest extends \PHPUnit_Framework_TestCase
 
 	public function __construct()
 	{
-		include_once( dirname( dirname( dirname( __FILE__ ) ) ) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Html' . DIRECTORY_SEPARATOR . 'HtmlEntities.php' );
+		//include_once( dirname( dirname( dirname( __FILE__ ) ) ) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Html' . DIRECTORY_SEPARATOR . 'HtmlEntities.php' );
 		$this->unencoded = '@,;:';
 		$this->encoded = '&amp;percnt;&amp;ast;&amp;lpar;&amp;num;';
 		$this->TestClass = new \GGG\Html\HtmlEntities();
