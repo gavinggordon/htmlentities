71ff3b40c085cc6f0ff822cf97c1f72d44399844
Gavin G Gordon <gmarkowski@my.bcit.ca>
2016-04-03 18:33:03 -0700

    Updated .travis.yml files
    Attempt #16 to resolve issue with .travis.ci for PHPUnit testing of this
    class.

--- tests/Html/HtmlEntitiesTest.php
+++ tests/Html/HtmlEntitiesTest.php
@@ -1,6 +1,10 @@
 <?php
 
-class SameTest extends \PHPUnit_Framework_TestCase
+namespace GGGTests;
+
+use GGG\Html\HtmlEntities;
+
+class HtmlEntitiesTest extends \PHPUnit_Framework_TestCase
 {
 
 	private $TestClass = NULL;
@@ -11,17 +15,17 @@ class SameTest extends \PHPUnit_Framework_TestCase
 	{
 		$this->unencoded = '@,;:';
 		$this->encoded = '&amp;percnt;&amp;ast;&amp;lpar;&amp;num;';
-		$this->TestClass = new \GGG\Html\HtmlEntities;
+		$this->TestClass = new HtmlEntities;
 	}
 
-	public function testFailure()
+	public function testEncodeEntities()
     {
 		$actual = $this->TestClass->encode( $this->unencoded );
 		$expected = '&amp;commat;&amp;comma;&amp;semi;&amp;colon;';
         $this->assertSame( $expected, $actual );
     }
 	
-	public function testFailure2()
+	public function testDecodeEntities()
     {
 		$actual = $this->TestClass->decode( $this->encoded );
 		$expected = '%*(#';
--- tests/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once( __DIR__  . DIRECTORY_SEPARATOR . 'bootstrap.php' );
-include_once( __DIR__  . DIRECTORY_SEPARATOR . 'SameTest.php' );
-
-$test = new SameTest();
